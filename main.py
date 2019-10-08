6352a8ace4793e610d9d6d1f363ea87d7d50a55a
Filip <filipcoja@gmail.com>
2019-10-08 18:06:15 +0200

    [FEATURE] Added main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+
+# Path to images of bills
+BILL_IMG_DIR = 'C:/Users/Filip/Desktop/Projekte/Diplomarbeit/ai/bills'
+IMG_SIZE = 3000
+
+training_data = []
+
+
+# Iterate through directory with images of bills
+for img in os.listdir(BILL_IMG_DIR):
+    # Read image as array
+    img_pixels_array = cv2.imread(os.path.join(BILL_IMG_DIR, img), cv2.IMREAD_GRAYSCALE)
+    print(img_pixels_array.shape)
+
+    resized_pixels_array = cv2.resize(img_pixels_array, (IMG_SIZE, IMG_SIZE))
+
+    ### Print image in plot
+    plt.imshow(img_pixels_array, cmap="gray")
+    plt.show()
+
+    training_data.append([resized_pixels_array, []])
+
+
+
