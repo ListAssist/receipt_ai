6b5e1ed18fd6a0e4135415e1980542008a611f5f
Filip <filipcoja@gmail.com>
2019-10-08 21:47:20 +0200

    [FEATURE] created temporary model and started preprocessing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import cv2
+import pickle
 
 # Path to images of bills
 BILL_IMG_DIR = 'C:/Users/Filip/Desktop/Projekte/Diplomarbeit/ai/bills'
@@ -19,10 +20,22 @@ for img in os.listdir(BILL_IMG_DIR):
     resized_pixels_array = cv2.resize(img_pixels_array, (IMG_SIZE, IMG_SIZE))
 
     ### Print image in plot
-    plt.imshow(img_pixels_array, cmap="gray")
+    plt.imshow(resized_pixels_array, cmap="gray")
     plt.show()
 
     training_data.append([resized_pixels_array, []])
+
+X = []
+Y = []
+
+for x, y in training_data:
+    X.append(x)
+    y.append(y)
+
+X = np.array(X).reshape((-1, IMG_SIZE, IMG_SIZE, 1))
+
+pickle.dump(X, open("X.pickle", "wb"))
+pickle.dump(Y, open("Y.pickle", "wb"))
 
 
 
