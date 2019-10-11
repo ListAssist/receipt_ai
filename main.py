ea874d7ae3e9180885ffc3ac70986f027b072ad1
Filip <filipcoja@gmail.com>
2019-10-11 17:44:38 +0200

    [FEATURE] added adaptive thresholding for binarification of pictures

--- main.py
+++ main.py
@@ -4,38 +4,48 @@ import os
 import cv2
 import pickle
 
-# Path to images of bills
-BILL_IMG_DIR = 'C:/Users/Filip/Desktop/Projekte/Diplomarbeit/ai/bills'
+# path to images of bills
+BILL_IMG_DIR = "bills"
+FULL_PATH = os.path.join(os.getcwd(), BILL_IMG_DIR)
+
+# fixed pixel size to resize to
 IMG_SIZE = 3000
 
 training_data = []
 
-
-# Iterate through directory with images of bills
-for img in os.listdir(BILL_IMG_DIR):
-    # Read image as array
-    img_pixels_array = cv2.imread(os.path.join(BILL_IMG_DIR, img), cv2.IMREAD_GRAYSCALE)
-    print(img_pixels_array.shape)
-
+# iterate through directory with images of bills
+for img in os.listdir(FULL_PATH):
+    # read image pixels in array as grayscale values
+    print(os.path.join(FULL_PATH, img))
+    img_pixels_array = cv2.imread(os.path.join(FULL_PATH, img), cv2.IMREAD_GRAYSCALE)
     resized_pixels_array = cv2.resize(img_pixels_array, (IMG_SIZE, IMG_SIZE))
+    binary_pixels_array = cv2.adaptiveThreshold(resized_pixels_array,
+                                                255,
+                                                cv2.ADAPTIVE_THRESH_MEAN_C,
+                                                cv2.THRESH_BINARY,
+                                                11,
+                                                5)
 
-    ### Print image in plot
-    plt.imshow(resized_pixels_array, cmap="gray")
+    # print image in plot
+    plt.imshow(binary_pixels_array, cmap="gray")
     plt.show()
 
+    # TODO: Load labels from labelbox
     training_data.append([resized_pixels_array, []])
 
 X = []
 Y = []
 
+# crete sperate arrays for features and labels
 for x, y in training_data:
     X.append(x)
     y.append(y)
 
 X = np.array(X).reshape((-1, IMG_SIZE, IMG_SIZE, 1))
 
-pickle.dump(X, open("X.pickle", "wb"))
-pickle.dump(Y, open("Y.pickle", "wb"))
+
+#pickle.dump(X, open("X.pickle", "wb"))
+#pickle.dump(Y, open("Y.pickle", "wb"))
 
 
 
