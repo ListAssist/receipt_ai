3465f63e7166972b6359c7f5bff162f7a26b72f8
Filip <filipcoja@gmail.com>
2019-11-04 18:13:57 +0100

    [FEATURE] four point transformation on flask server

--- preprocessing.py
+++ preprocessing.py
@@ -47,7 +47,7 @@ def calculate_distance(a, b):
 def four_point_transform(points, image):
     top_left = (points[0], points[1])
     bottom_left = (points[2], points[3])
-    bottom_right =(points[4], points[5])
+    bottom_right = (points[4], points[5])
     top_right = (points[6], points[7])
 
     # Take largest width as final width of cutout image
@@ -68,78 +68,9 @@ def four_point_transform(points, image):
     return cv2.warpPerspective(image, M, (max_width, max_height))
 
 
-# Get minimum bounding rect for a set of points (my quadliteral!)
-# Taken from https://gis.stackexchange.com/questions/22895/finding-minimum-area-rectangle-for-given-points/169633#169633
-def minimum_bounding_rectangle(points):
-    """
-    Find the smallest bounding rectangle for a set of points.
-    Returns a set of points representing the corners of the bounding box.
-
-    :param points: an nx2 matrix of coordinates
-    :rval: an nx2 matrix of coordinates
-    """
-    from scipy.ndimage.interpolation import rotate
-    pi2 = np.pi / 2.
-
-    # get the convex hull for the points
-    hull_points = points[ConvexHull(points).vertices]
-
-    # calculate edge angles
-    edges = np.zeros((len(hull_points) - 1, 2))
-    edges = hull_points[1:] - hull_points[:-1]
-
-    angles = np.zeros((len(edges)))
-    angles = np.arctan2(edges[:, 1], edges[:, 0])
-
-    angles = np.abs(np.mod(angles, pi2))
-    angles = np.unique(angles)
-
-    # find rotation matrices
-    # XXX both work
-    rotations = np.vstack([
-        np.cos(angles),
-        np.cos(angles - pi2),
-        np.cos(angles + pi2),
-        np.cos(angles)]).T
-    #     rotations = np.vstack([
-    #         np.cos(angles),
-    #         -np.sin(angles),
-    #         np.sin(angles),
-    #         np.cos(angles)]).T
-    rotations = rotations.reshape((-1, 2, 2))
-
-    # apply rotations to the hull
-    rot_points = np.dot(rotations, hull_points.T)
-
-    # find the bounding points
-    min_x = np.nanmin(rot_points[:, 0], axis=1)
-    max_x = np.nanmax(rot_points[:, 0], axis=1)
-    min_y = np.nanmin(rot_points[:, 1], axis=1)
-    max_y = np.nanmax(rot_points[:, 1], axis=1)
-
-    # find the box with the best area
-    areas = (max_x - min_x) * (max_y - min_y)
-    best_idx = np.argmin(areas)
-
-    # return the best box
-    x1 = max_x[best_idx]
-    x2 = min_x[best_idx]
-    y1 = max_y[best_idx]
-    y2 = min_y[best_idx]
-    r = rotations[best_idx]
-
-    rval = np.zeros((4, 2))
-    rval[0] = np.dot([x1, y2], r)
-    rval[1] = np.dot([x2, y2], r)
-    rval[2] = np.dot([x2, y1], r)
-    rval[3] = np.dot([x1, y1], r)
-
-    return rval
-
-
 # Algorithm to skew image correctly
 # Only works on well photographed pictures of receipts
-def skewAlgorithm(image):
+def skew_algorithm(image):
     # edge detection
     sigma = 0.33
     # get average grayscale value
@@ -186,7 +117,7 @@ if __name__ == "__main__":
             # extra important data
             IMG_NAME = label["External ID"]
             IMPORTANT_VERTICES = label["Label"]["important"][0]["geometry"]
-            #BILL_VERTICES = label["Label"]["bill"][0]["geometry"]
+            #### BILL_VERTICES = label["Label"]["bill"][0]["geometry"]
 
             # read image pixels in 2d array as grayscale values
             img = name_to_img[IMG_NAME]
--- server.py
+++ server.py
@@ -1,18 +1,40 @@
+import numpy as np
 from flask import Flask, request
 from flask_cors import CORS
-from werkzeug.datastructures import ImmutableMultiDict
 import cv2
 from flask import Response
+from werkzeug.datastructures import ImmutableMultiDict
+from preprocessing import calculate_distance, four_point_transform
+import json
 
 app = Flask(__name__)
 CORS(app)
 
 
+# creates one dim array from points dictionary
+def transform_to_1d(points_as_dict):
+    one_dim_coordinates = []
+    for point in points_as_dict:
+        one_dim_coordinates.append(point["x"])
+        one_dim_coordinates.append(point["y"])
+    return one_dim_coordinates
+
+
 @app.route('/', methods=["POST"])
 def main():
+    # retrieve parameters (in this case coordinates) from request
     data = dict(request.form)
-    cv2.imshow("king", data["bild"])
+    coordinates = json.loads(data["coordinates"][0])
+
+    # Decode image which was send from flutter with multipart form data
+    img = cv2.imdecode(np.frombuffer(request.files["bill"].read(), np.uint8), cv2.IMREAD_UNCHANGED)
+
+    cv2.imshow("king", four_point_transform(transform_to_1d(coordinates), img))
+    cv2.waitKey(0)
     return Response(status=201)
 
 
-app.run(Debug=True, port=5000)
+app.run(debug=True, port=5000)
+
+
+
