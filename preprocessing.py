96e93d0e9d7810f876a894110b69d8f2fcc5f127
filip <>
2019-11-25 17:12:46 +0100

    [FEATURE] added augmentation

--- preprocessing.py
+++ preprocessing.py
@@ -33,9 +33,11 @@ training_data = []
 def transform_vertices(vertices, ratio):
     output_vertices = []
     # get coordinates for fixed size picture and squash them between 0 and 1
-    for vertice in vertices:
-        output_vertices.append(vertice["x"] * ratio[0])
-        output_vertices.append(vertice["y"] * ratio[1])
+    for i, vertice in enumerate(vertices):
+        if i % 2 == 0:
+            output_vertices.append(vertice * ratio[0])
+        else:
+            output_vertices.append(vertice * ratio[1])
     return output_vertices
 
 
@@ -124,76 +126,19 @@ def skew_algorithm(image):
 if __name__ == "__main__":
     seq = iaa.Sequential(
         [
-            # apply the following augmenters to most images
-            iaa.Fliplr(0.5),  # horizontally flip 50% of all images
-            iaa.Flipud(0.2),  # vertically flip 20% of all images
-            # crop images by -5% to 10% of their height/width
-            sometimes(iaa.CropAndPad(
-                percent=(-0.05, 0.1),
-                pad_mode=ia.ALL,
-                pad_cval=(0, 255)
-            )),
+            iaa.Fliplr(0.5),
             sometimes(iaa.Affine(
                 scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
                 # scale images to 80-120% of their size, individually per axis
                 translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},  # translate by -20 to +20 percent (per axis)
-                rotate=(-45, 45),  # rotate by -45 to +45 degrees
+                rotate=(-30, 30),  # rotate by -45 to +45 degrees
                 shear=(-16, 16),  # shear by -16 to +16 degrees
                 order=[0, 1],  # use nearest neighbour or bilinear interpolation (fast)
                 cval=(0, 255),  # if mode is constant, use a cval between 0 and 255
                 mode=ia.ALL  # use any of scikit-image's warping modes (see 2nd image from the top for examples)
             )),
-            # execute 0 to 5 of the following (less important) augmenters per image
-            # don't execute all of them, as that would often be way too strong
-            iaa.SomeOf((0, 5),
-                       [
-                           sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))),
-                           # convert images into their superpixel representation
-                           iaa.OneOf([
-                               iaa.GaussianBlur((0, 3.0)),  # blur images with a sigma between 0 and 3.0
-                               iaa.AverageBlur(k=(2, 7)),
-                               # blur image using local means with kernel sizes between 2 and 7
-                               iaa.MedianBlur(k=(3, 11)),
-                               # blur image using local medians with kernel sizes between 2 and 7
-                           ]),
-                           iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),  # sharpen images
-                           iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)),  # emboss images
-                           # search either for all edges or for directed edges,
-                           # blend the result with the original image using a blobby mask
-                           iaa.SimplexNoiseAlpha(iaa.OneOf([
-                               iaa.EdgeDetect(alpha=(0.5, 1.0)),
-                               iaa.DirectedEdgeDetect(alpha=(0.5, 1.0), direction=(0.0, 1.0)),
-                           ])),
-                           iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05 * 255), per_channel=0.5),
-                           # add gaussian noise to images
-                           iaa.OneOf([
-                               iaa.Dropout((0.01, 0.1), per_channel=0.5),  # randomly remove up to 10% of the pixels
-                               iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
-                           ]),
-                           iaa.Invert(0.05, per_channel=True),  # invert color channels
-                           iaa.Add((-10, 10), per_channel=0.5),
-                           # change brightness of images (by -10 to 10 of original value)
-                           iaa.AddToHueAndSaturation((-20, 20)),  # change hue and saturation
-                           # either change the brightness of the whole image (sometimes
-                           # per channel) or change the brightness of subareas
-                           iaa.OneOf([
-                               iaa.Multiply((0.5, 1.5), per_channel=0.5),
-                               iaa.FrequencyNoiseAlpha(
-                                   exponent=(-4, 0),
-                                   first=iaa.Multiply((0.5, 1.5), per_channel=True),
-                                   second=iaa.LinearContrast((0.5, 2.0))
-                               )
-                           ]),
-                           iaa.LinearContrast((0.5, 2.0), per_channel=0.5),  # improve or worsen the contrast
-                           iaa.Grayscale(alpha=(0.0, 1.0)),
-                           sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)),
-                           # move pixels locally around (with random strengths)
-                           sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))),
-                           # sometimes move parts of the image around
-                           sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
-                       ],
-                       random_order=True
-                       )
+            sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))), # sometimes move parts of the image around
+            sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
         ],
         random_order=True
     )
@@ -204,7 +149,7 @@ if __name__ == "__main__":
         # remove skipped labels
         data = [label for label in data if label["Label"] != "Skip"]
 
-        images = []
+        original_images = []
         polygons = []
         for label in tqdm(data):
             # CONSTANTS
@@ -212,28 +157,28 @@ if __name__ == "__main__":
             IMPORTANT_VERTICES = label["Label"]["important"][0]["geometry"]
             # BILL_VERTICES = label["Label"]["bill"][0]["geometry"]
 
-            raw_img = cv2.imread(os.path.join(FULL_PATH, IMG_NAME), cv2.IMREAD_COLOR)
-            cv2.imshow("hey", raw_img)
-            cv2.waitKey(1)
+            raw_img = cv2.imread(os.path.join(FULL_PATH, IMG_NAME), cv2.IMREAD_GRAYSCALE)
+
+            # load every picture as grayscale and create dictionary
+            original_images.append(raw_img)
+            poly = ia.Polygon(vertice_chunks(IMPORTANT_VERTICES))
+            polygons.append(poly)
+
+        images, polygons = seq(images=original_images, polygons=polygons)
+        for i, img in enumerate(images):
+            img_vertices = []
+            for idx in range(len(polygons[i].xx)):
+                img_vertices.append(polygons[i].xx[idx])
+                img_vertices.append(polygons[i].yy[idx])
+
             # resize image to fixed resolution
-            resized_img = cv2.resize(raw_img, (RES_X, RES_Y))
+            resized_img = cv2.resize(img, (RES_X, RES_Y))
 
             # calculate ratios to transform labeled coordinates to resized image
             RATIO_X = RES_X / raw_img.shape[1]
             RATIO_Y = RES_Y / raw_img.shape[0]
+            img_vertices = transform_vertices(img_vertices, (RATIO_X, RATIO_Y))
 
-            # load every picture as grayscale and create dictionary
-            images.append(resized_img)
-            polygons.append(ia.Polygon(list(chunks(transform_vertices(IMPORTANT_VERTICES, (RATIO_X, RATIO_Y)), 2))))
-
-        images, polygons = seq(images=np.array(images).reshape((len(images), RES_X, RES_Y, 3)), polygons=polygons)
-        for i, img in enumerate(images):
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-            vertices = []
-            for idx in range(len(polygons[i].xx)):
-                vertices.append(polygons[i].xx[idx])
-                vertices.append(polygons[i].yy[idx])
-            print(vertices)
             ''' create binary picture (just b&w)
             https://docs.opencv.org/2.4/modules/imgproc/doc/miscellaneous_transformations.html
             binary_img = cv2 \
@@ -244,17 +189,22 @@ if __name__ == "__main__":
             
             https://dsp.stackexchange.com/questions/2411/what-are-the-most-common-algorithms-for-adaptive-thresholding
             '''
-
             threshold = threshold_otsu(cv2.GaussianBlur(resized_img, (5, 5), 0))
             binary_img = resized_img > threshold
-
+            print(img.shape)
             # Show polygons (coordinates must not be squashed)
             if DEBUG:
-                plt.imshow(np.array(np.array(img).reshape((RES_X, RES_Y))), cmap='gray', interpolation='bicubic')
-                plt.show()
-                cv2.imshow("transformed", four_point_transform(vertices, img))
+                cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+                cv2.resizeWindow("image", 600, 600)
+                cv2.imshow("image", resized_img)
+                cv2.waitKey(0)
 
-            training_output_vertices = normalize_vertices(vertices)
+                skew_algorithm(binary_img)
+                # plt.imshow(four_point_transform(img_vertices, resized_img), cmap="gray")
+                plt.imshow(binary_img, cmap="gray")
+                plt.show()
+
+            training_output_vertices = normalize_vertices(img_vertices)
             training_data.append([binary_img, training_output_vertices])
 
     X = []
@@ -265,7 +215,6 @@ if __name__ == "__main__":
         X.append(x)
         Y.append(y)
 
-    print(tabulate(training_data))
     X = np.array(X).reshape((-1, RES_X, RES_Y, 1))
     Y = np.array(Y)
 
