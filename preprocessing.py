45accd0c1363467425cd65cf077a3dfaee90bc31
filip <>
2019-11-25 21:40:39 +0100

    [FEATURE] added edge detection

--- preprocessing.py
+++ preprocessing.py
@@ -11,6 +11,7 @@ from skimage.filters import threshold_otsu
 from tqdm import tqdm
 import imgaug.augmenters as iaa
 import imgaug as ia
+from scipy.spatial.distance import cdist
 
 sometimes = lambda aug: iaa.Sometimes(0.5, aug)
 
@@ -90,36 +91,62 @@ def four_point_transform(points, image):
     return cv2.warpPerspective(image, M, (max_width, max_height))
 
 
+# https://github.com/jrosebr1/imutils/blob/master/imutils/perspective.py
+def order_points(pts):
+    # sort the points based on their x-coordinates
+    xSorted = pts[np.argsort(pts[:, 0]), :]
+
+    # grab the left-most and right-most points from the sorted
+    # x-roodinate points
+    leftMost = xSorted[:2, :]
+    rightMost = xSorted[2:, :]
+
+    # now, sort the left-most coordinates according to their
+    # y-coordinates so we can grab the top-left and bottom-left
+    # points, respectively
+    leftMost = leftMost[np.argsort(leftMost[:, 1]), :]
+    (tl, bl) = leftMost
+
+    # now that we have the top-left coordinate, use it as an
+    # anchor to calculate the Euclidean distance between the
+    # top-left and right-most points; by the Pythagorean
+    # theorem, the point with the largest distance will be
+    # our bottom-right point
+    D = cdist(tl[np.newaxis], rightMost, "euclidean")[0]
+    (br, tr) = rightMost[np.argsort(D)[::-1], :]
+
+    # return the coordinates in top-left, top-right,
+    # bottom-right, and bottom-left order
+    return np.array([tl, bl, br, tr], dtype="float32")
+
+
 # Algorithm to skew image correctly
 # Only works on well photographed pictures of receipts
-def skew_algorithm(image):
-    # edge detection
-    sigma = 0.33
-    # get average grayscale value
-    median = np.median(image)
-    # create limit for aperture size (kernel size)
-    lower = int(max(0, (1.0 - sigma) * median))
-    upper = int(min(255, (1.0 + sigma) * median))
-
-    # blur image to focus on edges and shapes
-    img_blurred = cv2.GaussianBlur(image, (5, 5), 0)
-    # detect edges with the Canny86 Algorithm
-    img_blurred = cv2.Canny(img_blurred, lower, upper)
-    # cv2.imshow("Blurred img", img_blurred)
-    # cv2.waitKey(0)
-
+def edge_detection(image):
     # find contours
-    contours = cv2.findContours(img_blurred.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     contours = imutils.grab_contours(contours)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
-
     for contour in contours:
+        if cv2.contourArea(contour) < 100:
+            continue
+
         peri = cv2.arcLength(contour, True)
         approximation = cv2.approxPolyDP(contour, 0.02 * peri, True)
+
+        # bbox less precise
+        # bbox = cv2.minAreaRect(contour)
+        # approximation = np.array(cv2.boxPoints(bbox))
         if len(approximation) == 4:
-            cv2.drawContours(image, [approximation], -1, (0, 255, 0), 2)
-            cv2.imshow("Outline", image)
+            approximation = np.array(approximation).reshape((4, 2))
+
+            cv2.namedWindow("main", cv2.WINDOW_NORMAL)
+            cv2.resizeWindow("main", 600, 600)
+            cv2.imshow("main", four_point_transform(order_points(approximation).flatten(), image))
             cv2.waitKey(0)
+            cv2.imshow("main", image)
+            cv2.waitKey(0)
+
             break
 
 
@@ -137,7 +164,7 @@ if __name__ == "__main__":
                 cval=(0, 255),  # if mode is constant, use a cval between 0 and 255
                 mode=ia.ALL  # use any of scikit-image's warping modes (see 2nd image from the top for examples)
             )),
-            sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))), # sometimes move parts of the image around
+            sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))),  # sometimes move parts of the image around
             sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
         ],
         random_order=True
@@ -149,7 +176,7 @@ if __name__ == "__main__":
         # remove skipped labels
         data = [label for label in data if label["Label"] != "Skip"]
 
-        original_images = []
+        images = []
         polygons = []
         for label in tqdm(data):
             # CONSTANTS
@@ -160,16 +187,16 @@ if __name__ == "__main__":
             raw_img = cv2.imread(os.path.join(FULL_PATH, IMG_NAME), cv2.IMREAD_GRAYSCALE)
 
             # load every picture as grayscale and create dictionary
-            original_images.append(raw_img)
+            images.append(raw_img)
             poly = ia.Polygon(vertice_chunks(IMPORTANT_VERTICES))
-            polygons.append(poly)
+            polygons.append([poly])
 
-        images, polygons = seq(images=original_images, polygons=polygons)
+        # images, polygons = seq(images=images, polygons=polygons)
         for i, img in enumerate(images):
             img_vertices = []
-            for idx in range(len(polygons[i].xx)):
-                img_vertices.append(polygons[i].xx[idx])
-                img_vertices.append(polygons[i].yy[idx])
+            for idx in range(len(polygons[i][0].xx)):
+                img_vertices.append(polygons[i][0].xx[idx])
+                img_vertices.append(polygons[i][0].yy[idx])
 
             # resize image to fixed resolution
             resized_img = cv2.resize(img, (RES_X, RES_Y))
@@ -189,23 +216,17 @@ if __name__ == "__main__":
             
             https://dsp.stackexchange.com/questions/2411/what-are-the-most-common-algorithms-for-adaptive-thresholding
             '''
-            threshold = threshold_otsu(cv2.GaussianBlur(resized_img, (5, 5), 0))
-            binary_img = resized_img > threshold
-            print(img.shape)
+            ret, b_w_image = cv2.threshold(resized_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
             # Show polygons (coordinates must not be squashed)
             if DEBUG:
-                cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-                cv2.resizeWindow("image", 600, 600)
-                cv2.imshow("image", resized_img)
-                cv2.waitKey(0)
-
-                skew_algorithm(binary_img)
+                edge_detection(b_w_image)
                 # plt.imshow(four_point_transform(img_vertices, resized_img), cmap="gray")
-                plt.imshow(binary_img, cmap="gray")
-                plt.show()
+                # plt.imshow(thresh, cmap="gray")
+                # plt.show()
 
             training_output_vertices = normalize_vertices(img_vertices)
-            training_data.append([binary_img, training_output_vertices])
+            training_data.append([b_w_image, training_output_vertices])
 
     X = []
     Y = []
