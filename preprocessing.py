e6da47902c421bed0ee3540f673eafae0672758d
Filip <filipcoja@gmail.com>
2019-11-25 15:55:02 +0100

    [FIX] started data augmentation

--- preprocessing.py
+++ preprocessing.py
@@ -9,7 +9,10 @@ from tabulate import tabulate
 from matplotlib import patches
 from skimage.filters import threshold_otsu
 from tqdm import tqdm
+import imgaug.augmenters as iaa
+import imgaug as ia
 
+sometimes = lambda aug: iaa.Sometimes(0.5, aug)
 
 # if true will print plots and information about preprocessing
 DEBUG = True
@@ -31,9 +34,29 @@ def transform_vertices(vertices, ratio):
     output_vertices = []
     # get coordinates for fixed size picture and squash them between 0 and 1
     for vertice in vertices:
-        output_vertices.append(vertice["x"] * ratio[0] / (1 if DEBUG else RES_X))
-        output_vertices.append(vertice["y"] * ratio[1] / (1 if DEBUG else RES_Y))
+        output_vertices.append(vertice["x"] * ratio[0])
+        output_vertices.append(vertice["y"] * ratio[1])
     return output_vertices
+
+
+def vertice_chunks(vertices):
+    output_vertices = []
+
+    for vertice in vertices:
+        output_vertices.append((vertice["x"], vertice["y"]))
+    return output_vertices
+
+
+# create chunks
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+# normalize vertices
+def normalize_vertices(vertices):
+    return [vertice / (RES_X if index % 2 == 0 else RES_Y) for index, vertice in enumerate(vertices)]
 
 
 # distance between two points with pythagoras
@@ -99,36 +122,118 @@ def skew_algorithm(image):
 
 
 if __name__ == "__main__":
-    # load every picture as grayscale and create dictionary
-    name_to_img = {}
-    for img in tqdm(os.listdir(FULL_PATH)):
-        name_to_img[img] = cv2.imread(os.path.join(FULL_PATH, img), cv2.IMREAD_GRAYSCALE)
+    seq = iaa.Sequential(
+        [
+            # apply the following augmenters to most images
+            iaa.Fliplr(0.5),  # horizontally flip 50% of all images
+            iaa.Flipud(0.2),  # vertically flip 20% of all images
+            # crop images by -5% to 10% of their height/width
+            sometimes(iaa.CropAndPad(
+                percent=(-0.05, 0.1),
+                pad_mode=ia.ALL,
+                pad_cval=(0, 255)
+            )),
+            sometimes(iaa.Affine(
+                scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                # scale images to 80-120% of their size, individually per axis
+                translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},  # translate by -20 to +20 percent (per axis)
+                rotate=(-45, 45),  # rotate by -45 to +45 degrees
+                shear=(-16, 16),  # shear by -16 to +16 degrees
+                order=[0, 1],  # use nearest neighbour or bilinear interpolation (fast)
+                cval=(0, 255),  # if mode is constant, use a cval between 0 and 255
+                mode=ia.ALL  # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+            )),
+            # execute 0 to 5 of the following (less important) augmenters per image
+            # don't execute all of them, as that would often be way too strong
+            iaa.SomeOf((0, 5),
+                       [
+                           sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))),
+                           # convert images into their superpixel representation
+                           iaa.OneOf([
+                               iaa.GaussianBlur((0, 3.0)),  # blur images with a sigma between 0 and 3.0
+                               iaa.AverageBlur(k=(2, 7)),
+                               # blur image using local means with kernel sizes between 2 and 7
+                               iaa.MedianBlur(k=(3, 11)),
+                               # blur image using local medians with kernel sizes between 2 and 7
+                           ]),
+                           iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),  # sharpen images
+                           iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)),  # emboss images
+                           # search either for all edges or for directed edges,
+                           # blend the result with the original image using a blobby mask
+                           iaa.SimplexNoiseAlpha(iaa.OneOf([
+                               iaa.EdgeDetect(alpha=(0.5, 1.0)),
+                               iaa.DirectedEdgeDetect(alpha=(0.5, 1.0), direction=(0.0, 1.0)),
+                           ])),
+                           iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05 * 255), per_channel=0.5),
+                           # add gaussian noise to images
+                           iaa.OneOf([
+                               iaa.Dropout((0.01, 0.1), per_channel=0.5),  # randomly remove up to 10% of the pixels
+                               iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
+                           ]),
+                           iaa.Invert(0.05, per_channel=True),  # invert color channels
+                           iaa.Add((-10, 10), per_channel=0.5),
+                           # change brightness of images (by -10 to 10 of original value)
+                           iaa.AddToHueAndSaturation((-20, 20)),  # change hue and saturation
+                           # either change the brightness of the whole image (sometimes
+                           # per channel) or change the brightness of subareas
+                           iaa.OneOf([
+                               iaa.Multiply((0.5, 1.5), per_channel=0.5),
+                               iaa.FrequencyNoiseAlpha(
+                                   exponent=(-4, 0),
+                                   first=iaa.Multiply((0.5, 1.5), per_channel=True),
+                                   second=iaa.LinearContrast((0.5, 2.0))
+                               )
+                           ]),
+                           iaa.LinearContrast((0.5, 2.0), per_channel=0.5),  # improve or worsen the contrast
+                           iaa.Grayscale(alpha=(0.0, 1.0)),
+                           sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)),
+                           # move pixels locally around (with random strengths)
+                           sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))),
+                           # sometimes move parts of the image around
+                           sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
+                       ],
+                       random_order=True
+                       )
+        ],
+        random_order=True
+    )
 
-    # open exported json and read labels
-    with open("labelbox_export.json", "r") as export_file:
+    with open("label_export.json", "r") as export_file:
         data = json.load(export_file)
 
         # remove skipped labels
         data = [label for label in data if label["Label"] != "Skip"]
-        for label in data:
-            # extra important data
+
+        images = []
+        polygons = []
+        for label in tqdm(data):
+            # CONSTANTS
             IMG_NAME = label["External ID"]
             IMPORTANT_VERTICES = label["Label"]["important"][0]["geometry"]
-            #### BILL_VERTICES = label["Label"]["bill"][0]["geometry"]
+            # BILL_VERTICES = label["Label"]["bill"][0]["geometry"]
 
-            # read image pixels in 2d array as grayscale values
-            img = name_to_img[IMG_NAME]
+            raw_img = cv2.imread(os.path.join(FULL_PATH, IMG_NAME), cv2.IMREAD_COLOR)
+            cv2.imshow("hey", raw_img)
+            cv2.waitKey(1)
+            # resize image to fixed resolution
+            resized_img = cv2.resize(raw_img, (RES_X, RES_Y))
 
             # calculate ratios to transform labeled coordinates to resized image
-            RATIO_X = RES_X / img.shape[1]
-            RATIO_Y = RES_Y / img.shape[0]
+            RATIO_X = RES_X / raw_img.shape[1]
+            RATIO_Y = RES_Y / raw_img.shape[0]
 
-            # transform labeled vertices to resized image
-            training_output_vertices = transform_vertices(IMPORTANT_VERTICES, (RATIO_X, RATIO_Y))
+            # load every picture as grayscale and create dictionary
+            images.append(resized_img)
+            polygons.append(ia.Polygon(list(chunks(transform_vertices(IMPORTANT_VERTICES, (RATIO_X, RATIO_Y)), 2))))
 
-            # resize image to fixed resolution
-            resized_img = cv2.resize(img, (RES_X, RES_Y))
-
+        images, polygons = seq(images=np.array(images).reshape((len(images), RES_X, RES_Y, 3)), polygons=polygons)
+        for i, img in enumerate(images):
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            vertices = []
+            for idx in range(len(polygons[i].xx)):
+                vertices.append(polygons[i].xx[idx])
+                vertices.append(polygons[i].yy[idx])
+            print(vertices)
             ''' create binary picture (just b&w)
             https://docs.opencv.org/2.4/modules/imgproc/doc/miscellaneous_transformations.html
             binary_img = cv2 \
@@ -139,13 +244,17 @@ if __name__ == "__main__":
             
             https://dsp.stackexchange.com/questions/2411/what-are-the-most-common-algorithms-for-adaptive-thresholding
             '''
+
             threshold = threshold_otsu(cv2.GaussianBlur(resized_img, (5, 5), 0))
             binary_img = resized_img > threshold
 
             # Show polygons (coordinates must not be squashed)
             if DEBUG:
-                cv2.imshow("transformed", four_point_transform(training_output_vertices, resized_img))
+                plt.imshow(np.array(np.array(img).reshape((RES_X, RES_Y))), cmap='gray', interpolation='bicubic')
+                plt.show()
+                cv2.imshow("transformed", four_point_transform(vertices, img))
 
+            training_output_vertices = normalize_vertices(vertices)
             training_data.append([binary_img, training_output_vertices])
 
     X = []
--- server.py
+++ server.py
@@ -10,58 +10,6 @@ import json
 import pytesseract
 import matplotlib.pyplot as plt
 
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/', methods=["POST"])
-def main():
-    # retrieve parameters (in this case coordinates) from request
-    data = dict(request.form)
-    coordinates = json.loads(data["coordinates"])
-
-    # Decode image which was send from flutter with multipart form data
-    img = cv2.imdecode(np.frombuffer(request.files["bill"].read(), np.uint8), cv2.IMREAD_COLOR)
-    # four point transformation on the picture with the give coordinates
-    cropped_img = four_point_transform(transform_to_1d(coordinates), img)
-    resized_img = cv2.resize(cropped_img, None, fx=2, fy=2)
-    gray_img = cv2.cvtColor(resized_img, cv2.COLOR_RGB2GRAY)
-    gray_img = cv2.medianBlur(gray_img, 3)
-
-    # https://scikit-image.org/docs/dev/auto_examples/segmentation/plot_thresholding.html
-    # https://www.freecodecamp.org/news/getting-started-with-tesseract-part-ii-f7f9a0899b3f/
-
-    # get text from tesseract ocr engine
-    lines = pytesseract.image_to_data(gray_img, lang="deu", output_type=Output.DICT)
-    boxes_and_text = []
-
-    draw_boxes(lines, gray_img)
-    return jsonify(tesseract_to_json(lines))
-
-
-# Draw bound boxes which were detected by tesseract
-def draw_boxes(lines: dict, img):
-    fig, ax = plt.subplots(1)
-    plt.imshow(img, cmap="gray")
-    for i in range(len(lines["conf"])):
-        rect = patches.Rectangle((lines["left"][i], lines["top"][i]), lines["width"][i], lines["height"][i], linewidth=1, edgecolor='r', facecolor='none')
-        ax.add_patch(rect)
-    plt.show()
-
-
-def tesseract_to_json(lines):
-    detections = []
-    for i in range(len(lines["conf"])):
-        confidence = float(lines["conf"][i]) if int(lines["conf"][i]) != -1 else 0.0
-        detections.append({
-            "x": lines["left"][i],
-            "y": lines["top"][i],
-            "width": lines["width"][i],
-            "height": lines["height"][i],
-            "text": lines["text"][i],
-            "confidence": confidence / 100
-        })
-    return {"detections": detections}
-
 
 # creates one dim array from points dictionary
 def transform_to_1d(points_as_dict):
@@ -72,7 +20,58 @@ def transform_to_1d(points_as_dict):
     return one_dim_coordinates
 
 
-app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app = Flask(__name__)
+    CORS(app)
+
+    @app.route('/', methods=["POST"])
+    def main():
+        # retrieve parameters (in this case coordinates) from request
+        data = dict(request.form)
+        coordinates = json.loads(data["coordinates"])
+
+        # Decode image which was send from flutter with multipart form data
+        img = cv2.imdecode(np.frombuffer(request.files["bill"].read(), np.uint8), cv2.IMREAD_COLOR)
+        # four point transformation on the picture with the give coordinates
+        cropped_img = four_point_transform(transform_to_1d(coordinates), img)
+        resized_img = cv2.resize(cropped_img, None, fx=2, fy=2)
+        gray_img = cv2.cvtColor(resized_img, cv2.COLOR_RGB2GRAY)
+        gray_img = cv2.medianBlur(gray_img, 3)
+
+        # https://scikit-image.org/docs/dev/auto_examples/segmentation/plot_thresholding.html
+        # https://www.freecodecamp.org/news/getting-started-with-tesseract-part-ii-f7f9a0899b3f/
+
+        # get text from tesseract ocr engine
+        lines = pytesseract.image_to_data(gray_img, lang="deu", output_type=Output.DICT)
+        boxes_and_text = []
+
+        draw_boxes(lines, gray_img)
+        return jsonify(tesseract_to_json(lines))
+
+    # Draw bound boxes which were detected by tesseract
+    def draw_boxes(lines: dict, img):
+        fig, ax = plt.subplots(1)
+        plt.imshow(img, cmap="gray")
+        for i in range(len(lines["conf"])):
+            rect = patches.Rectangle((lines["left"][i], lines["top"][i]), lines["width"][i], lines["height"][i], linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+        plt.show()
+
+    def tesseract_to_json(lines):
+        detections = []
+        for i in range(len(lines["conf"])):
+            confidence = float(lines["conf"][i]) if int(lines["conf"][i]) != -1 else 0.0
+            detections.append({
+                "x": lines["left"][i],
+                "y": lines["top"][i],
+                "width": lines["width"][i],
+                "height": lines["height"][i],
+                "text": lines["text"][i],
+                "confidence": confidence / 100
+            })
+        return {"detections": detections}
+
+    app.run(debug=True, port=5000)
 
 
 
